8979e8430570a894fe0715e72c10bf596faabc46
Nicolas Armenti <misterchispa@gmail.com>
2014-11-06 18:37:41 -0300

    Primeros 25

--- Euler/Module1.vb
+++ Euler/Module1.vb
@@ -1,18 +1,23 @@
 ﻿Module Module1
 
-    Sub Main()
-        Problem1()
-    End Sub
+    Sub Main() '(Problem 40)
+        Dim BigN As String = String.Empty
+        Dim Ans As BigInteger = 1
+        Dim i As Integer = 1
+        Do
+            BigN &= i.ToString
+            i += 1
+        Loop Until BigN.Length >= 1000000
 
+        Ans *= BigN.Substring(1, 1) * BigN.Substring(10, 1) * BigN.Substring(100, 1) * BigN.Substring(1000, 1) * BigN.Substring(10000, 1) * BigN.Substring(100000, 1) * BigN.Substring(1000000, 1)
+
+        Console.WriteLine(Ans)
+        Console.ReadKey()
+    End Sub
 End Module
 
 
 'Posibles: 
-'   37
-'   39
-'       a+b+c=120
-'       a^2+b^2=c^2
-'       a<=b<=c
 '   41
 '   42
 '   46
--- Euler/Recursos.vb
+++ Euler/Recursos.vb
@@ -9,13 +9,17 @@
     End Function
 
     Friend Function esPrimo(ByVal Num As Long, ByRef Primos As List(Of Long)) As Boolean
-        For Each Primo In Primos
-            If Primo * Primo <= Num Then
-                If (Num Mod Primo) = 0 Then
-                    Return False
+        If Num > 1 Then
+            For Each Primo In Primos
+                If Primo * Primo <= Num Then
+                    If (Num Mod Primo) = 0 Then
+                        Return False
+                    End If
                 End If
-            End If
-        Next
+            Next
+        Else
+            Return False
+        End If
         Return True
     End Function
 
@@ -43,6 +47,62 @@
                 Return False
             End If
         Next
+        Return True
+    End Function
+
+    Friend Function Factorial(ByVal Num As Integer) As BigInteger
+        Select Num
+            Case 1, 0
+                Return 1
+            Case Else
+                Return Factorial(Num - 1) * Num
+        End Select
+    End Function
+
+    Friend Function SumaDigitos(ByVal Num As String) As Integer
+        Dim Sum As ULong = 0
+
+        While Num.Length > 0
+            Sum += Num.Substring(0, 1)
+            Num = Num.Substring(1)
+        End While
+
+        Return Sum
+    End Function
+
+    Friend Function SumaDigitos(ByVal Num As String, ByVal Exp As Integer) As Integer
+        Dim Sum As ULong = 0
+
+        While Num.Length > 0
+            Sum += (CInt(Num.Substring(0, 1))) ^ Exp
+            Num = Num.Substring(1)
+        End While
+
+        Return Sum
+    End Function
+
+    Friend Function permutar(ByVal Numero As String) As String
+        Numero = (Numero.Substring(1) & Numero.Substring(0, 1))
+        Return Numero
+    End Function
+
+    Friend Function isPrimeTruncateRight(ByVal Numero As String, ByRef Primos As List(Of Long)) As Boolean
+        While Numero.Length > 0
+            If Not esPrimo(Numero, Primos) Then
+                Return False
+            End If
+            Numero = Numero.Substring(1)
+        End While
+        Return True
+    End Function
+
+    Friend Function isPrimeTruncateleft(ByVal Numero As String, ByRef Primos As List(Of Long)) As Boolean
+        While Numero.Length > 0
+            If Not esPrimo(Numero, Primos) Then
+                Return False
+            End If
+            Numero = Numero.Substring(0, Numero.Length - 1)
+        End While
         Return True
     End Function
 End Module
