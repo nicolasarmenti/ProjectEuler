b155ca98fd96b797be129f5d2dd1133eee3e287e
Nicolas Armenti <nicolasarmenti@gmail.com>
2014-11-14 14:28:45 -0300

    Cambiado el gitignore para incluir txt

--- Euler/Module1.vb
+++ Euler/Module1.vb
@@ -8,6 +8,34 @@
             For col As Integer = 0 To Matrix.GetUpperBound(1)
                 'Testear si se puede usar el ultimo y hacer la multiplicacion (una para cada direccion -4-)
                 'En cada una comparar math.max() y guardar
+                Dim sol As Integer = 1
+                If Not Matrix(row, col + 3) = Nothing Then 'Horizontal
+                    For i As Integer = 0 To 3
+                        sol *= Matrix(row, col + i)
+                    Next
+                    Ans = Math.Max(Ans, sol)
+                End If
+
+                If Not Matrix(row + 3, col) = Nothing Then 'Vertical
+                    For i As Integer = 0 To 3
+                        sol *= Matrix(row + i, col)
+                    Next
+                    Ans = Math.Max(Ans, sol)
+                End If
+
+                If Not Matrix(row + 3, col + 3) = Nothing Then 'Diagonal hacia la derecha
+                    For i As Integer = 0 To 3
+                        sol *= Matrix(row + i, col + i)
+                    Next
+                    Ans = Math.Max(Ans, sol)
+                End If
+
+                If Not Matrix(row - 3, col + 3) = Nothing Then 'Diagonal hacia la izquierda
+                    For i As Integer = 0 To 3
+                        sol *= Matrix(row - i, col + i)
+                    Next
+                    Ans = Math.Max(Ans, sol)
+                End If
             Next
         Next
 
